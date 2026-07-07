cfbd12b6f2cd4cba5c7ccb785357555fecf2765a
Aadam <aadamaftabpersonal@gmail.com>
2026-07-07 15:56:26 +0530

    pushing v1

--- student-portal/src/App.jsx
+++ student-portal/src/App.jsx
@@ -1,13 +1,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Reviewer from "./pages/Reviewer";
+import Contests from "./pages/Contests";
+import Weakness from "./pages/Weakness";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+      <Route path="/reviewer" element={<ProtectedRoute><Reviewer /></ProtectedRoute>} />
+      <Route path="/contests" element={<ProtectedRoute><Contests /></ProtectedRoute>} />
+      <Route path="/weakness" element={<ProtectedRoute><Weakness /></ProtectedRoute>} />
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
 }
